45b95c2379506c029bd85d5abe89a7e0af18272e
pavan16120 <pavan.12147ee013@gmail.com>
2021-09-07 12:53:40 +0530

    by using echo,printf  coomnad to print colors with their codes

--- 02-print.sh
+++ 02-print.sh
@@ -9,3 +9,24 @@
 ##syntex: echo <message to print>
 
 echo hello world
+
+##syntex for color or new lines : echo -e "MESSAGE/n/t/e"
+# echo -e option do need message to be quoted (Either single or double quotes) | (with quotes also work but with more syntaxing)
+
+# print multiple lines (\n), some tab space (\t)
+
+echo -e Hello world,\n\tWelcome
+
+# enable colors (\e)
+
+# syntex: echo -e "\e[COLOR-CODEmMESSAGE"
+# COLORS AND THEIR CODES ARE
+#   RED        31
+#   GREEN      32
+#   YELLOOW    33
+#   BLUE       34
+#   MAGENTA    35
+#   CYAN       36
+
+echo -e "\e[31mMESSAGE IN RED"
+echo -e "\e[33mMESSAGE IN YELLOW"
