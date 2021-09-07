7c74ad6c85337b434873dfe582aa69097f4e5e59
pavan16120 <pavan.12147ee013@gmail.com>
2021-09-07 12:14:00 +0530

    by using echo,printf coomnad to print messages

---
+++ 02-print.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+##printing somthing on screen by two comments
+# 1.echo
+# 2.printf
+
+#printf will have deal with more syntexs how ever echo is more discrptive and yet does all things thatprintf does
+
+##syntex: echo <message to print>
+
+echo hello world
