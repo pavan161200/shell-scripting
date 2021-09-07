c2a6c1d4f7977cedbd774fd769a7319027a0a837
pavan16120 <pavan.12147ee013@gmail.com>
2021-09-07 17:50:31 +0530

    VARIABLES

--- 03-vars.sh
+++ 03-vars.sh
@@ -2,7 +2,13 @@
 
 A=10
 echo A= $A
+DATE="04-12-16"
+echo welcome,today date is $DATE
 
 ## command subs
 NO_OF_USERS=$(who |wc -1)
 echo number of users = $NO_OF_USERS
+
+##date with command substitution
+DATE=$(date)
+
