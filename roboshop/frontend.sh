7411c3fce3d3b9902ef044812049591ca7a5e7eb
pavan16120 <pavan.12147ee013@gmail.com>
2021-09-08 15:46:23 +0530

    add frontend

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 LOG=/tmp/roboshop/log
+
 echo -e "Installing nginx -y\t\t...\t\e[32mdone\e[om"
-yum install nginx -y
+yum install nginx -y >$LOG
 ### 1. output from commands should not be displayed on screen
 ### 2. validate the command is successful or not
 
