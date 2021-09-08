af739e5708e9527bc1bdebe028fe8a2a923f41dd
pavan16120 <pavan.12147ee013@gmail.com>
2021-09-08 16:56:01 +0530

    add frontend

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -2,14 +2,14 @@
 LOG=/tmp/roboshop/log
 
 echo -e "Installing nginx -y\t\t...\t\e[32mdone\e[om"
-yum install nginx -y >$LOG
+yum install nginx -y >>$LOG
 ### 1. output from commands should not be displayed on screen
 ### 2. validate the command is successful or not
 
 echo "enable nginx"
-systemctl enable nginx >$LOG
+systemctl enable nginx >>$LOG
 
 echo "starting nginx"
-systemctl start nginx >$LOG
+systemctl start nginx >>$LOG
 
 
